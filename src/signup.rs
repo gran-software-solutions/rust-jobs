6fa3555eaef3c21470a082cd06c3c78f78f309b5
Greg Glazewski <g.glazewski@gmail.com>
2023-08-04 17:48:10 +0200

    adds post method

--- src/signup.rs
+++ src/signup.rs
@@ -1,24 +1,45 @@
-use actix_web::{Responder, HttpResponse, get, web};
+use actix_web::{Responder, HttpResponse, get, web, post};
 use sailfish::TemplateOnce;
 
 #[derive(TemplateOnce)]
 #[template(path = "signup.stpl")]
 struct SignupTemplate {
     title: String,
+    signedup: bool,
 }
 
 impl SignupTemplate {
-    fn new(title: &str) -> Self {
-        Self { title: String::from(title) }
+    fn new(title: &str, signedup: bool) -> Self {
+        Self {
+            title: String::from(title),
+            signedup
+        }
     }
+}
+
+#[post("/signup")]
+async fn signup_process_form() -> impl Responder {
+    HttpResponse::Ok()
+    .body(
+        SignupTemplate::new("Sign Up!", true)
+        .render_once()
+        .unwrap()
+    )
 }
 
 #[get("/signup")]
 async fn signup_form() -> impl Responder {
     HttpResponse::Ok()
-    .body(SignupTemplate::new("Sign Up!").render_once().unwrap())
+    .body(
+        SignupTemplate::new("Sign Up!", false)
+        .render_once()
+        .unwrap()
+    )
 }
 
 pub fn signup(cfg: &mut web::ServiceConfig) {
-    cfg.service(signup_form);
+    cfg
+    .service(signup_form)
+    .service(signup_process_form)
+    ;
 }

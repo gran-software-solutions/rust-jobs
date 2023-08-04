9faf5b3b2781ded3819a32fb3ec790f0eb8bd3fc
Greg <113454525+gglazewskigran@users.noreply.github.com>
2023-08-04 16:19:57 +0200

    relocate code (#2)
    * relocate code
    
    * restructure code
    
    * use config instead
    
    ---------
    
    Co-authored-by: Greg Glazewski <g.glazewski@gmail.com>

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,7 @@
-use crate::Job;
+use crate::jobs::structs::Job;
+
 use uuid::Uuid;
+
 pub struct Db {
     pub jobs: Vec<Job>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,12 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use sailfish::TemplateOnce;
-use serde::Deserialize;
+mod jobs;
+
+use actix_web::{web, App, HttpServer};
 
 mod database;
 use database::*;
 use std::sync::Mutex;
-use uuid::Uuid;
 
-#[derive(Deserialize)]
-pub struct Job {
-    id: String,
-    title: String,
-}
-
-#[derive(TemplateOnce)]
-#[template(path = "home.stpl")]
-struct Home<'a> {
-    jobs: &'a Vec<Job>,
-}
-
-#[derive(TemplateOnce)]
-#[template(path = "job.stpl")]
-struct JobDetails<'a> {
-    job: &'a Job,
-}
-
-async fn homepage(db_mutex: web::Data<Mutex<Db>>) -> impl Responder {
-    let db = db_mutex.lock().unwrap();
-    HttpResponse::Ok().body(Home { jobs: db.get_all() }.render_once().unwrap())
-}
-async fn job_details(id: web::Path<Uuid>, db_mutex: web::Data<Mutex<Db>>) -> impl Responder {
-    let job_id = id.into_inner();
-    let db = db_mutex.lock().unwrap();
-    HttpResponse::Ok().body(
-        JobDetails {
-            job: db.get_job(job_id).expect("Expected to find a job"),
-        }
-        .render_once()
-        .unwrap(),
-    )
-}
+use crate::jobs::handlers::job_routes;
 
 #[actix_web::main]
 async fn main() {
@@ -50,8 +17,7 @@ async fn main() {
     let server = HttpServer::new(move || {
         App::new()
             .app_data(db.clone())
-            .route("/", web::get().to(homepage))
-            .route("/jobs/{job_id}", web::get().to(job_details))
+            .configure(job_routes)
     })
     .bind(addr)
     .unwrap()

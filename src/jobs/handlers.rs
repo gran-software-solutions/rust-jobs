f42732aea792a1f43c9083609080b71e0bd9b832
Greg Glazewski <g.glazewski@gmail.com>
2023-08-04 16:54:51 +0200

    add head component

--- src/jobs/handlers.rs
+++ src/jobs/handlers.rs
@@ -11,16 +11,22 @@ use super::structs::{Home, JobDetails};
 #[get("/")]
 async fn homepage(db_mutex: web::Data<Mutex<Db>>) -> impl Responder {
     let db = db_mutex.lock().unwrap();
-    HttpResponse::Ok().body(Home { jobs: db.get_all() }.render_once().unwrap())
+    HttpResponse::Ok().body(Home {
+         jobs: db.get_all(),
+         title: "Rust Jobs",
+        }.render_once().unwrap())
 }
 
 #[get("/jobs/{id}")]
 async fn job_details(id: web::Path<Uuid>, db_mutex: web::Data<Mutex<Db>>) -> impl Responder {
     let job_id = id.into_inner();
     let db = db_mutex.lock().unwrap();
+    let job = db.get_job(job_id).expect("Expected to find a job");
+
     HttpResponse::Ok().body(
         JobDetails {
-            job: db.get_job(job_id).expect("Expected to find a job"),
+            title: &job.title[..],
+            job: job,
         }
         .render_once()
         .unwrap(),
--- src/jobs/structs.rs
+++ src/jobs/structs.rs
@@ -10,11 +10,13 @@ pub struct Job {
 #[derive(TemplateOnce)]
 #[template(path = "home.stpl")]
 pub struct Home<'a> {
+    pub title: &'a str,
     pub jobs: &'a Vec<Job>,
 }
 
 #[derive(TemplateOnce)]
 #[template(path = "job.stpl")]
 pub struct JobDetails<'a> {
+    pub title: &'a str,
     pub job: &'a Job,
 }

fe24597b7e4d1e19bba8f88014ebcdf42e8bd379
Greg Glazewski <g.glazewski@gmail.com>
2023-08-06 13:07:19 +0200

    decouple rendering from handlers

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rust-jobs
+
+To enable debug logs use env var like so:
+`RUST_LOG=debug cargo run`
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-mod jobs;
-mod signup;
 mod database;
-mod static_files;
+mod jobs;
 mod k8s_probes;
+mod presenters;
+mod signup;
+mod static_files;
 
-use actix_web::{web, App, HttpServer, middleware};
+use actix_web::{
+    middleware,
+    web,
+    App, HttpServer,
+};
 use database::*;
-use static_files::*;
 use k8s_probes::*;
 use signup::*;
+use static_files::*;
 use std::sync::Mutex;
 
 use crate::jobs::handlers::job_routes;
 
 #[actix_web::main]
 async fn main() {
+    env_logger::init();
+
     let addr = "localhost:8080";
 
     let db = web::Data::new(Mutex::new(Db::new()));
@@ -34,10 +41,8 @@ async fn main() {
 }
 
 fn routes(cfg: &mut web::ServiceConfig) {
-    cfg
-    .configure(static_files)
-    .configure(job_routes)
-    .configure(k8s_probes)
-    .configure(signup)
-    ;
+    cfg.configure(static_files)
+        .configure(job_routes)
+        .configure(k8s_probes)
+        .configure(signup);
 }
